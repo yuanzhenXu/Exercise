c26ba49be94fdecdbab249a773e536126265363b
yuanzhenXu <479359453@qq.com>
2017-06-25 11:13:06 +0800

    add relationships controller

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module Exercise
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.sass.preferred_syntax= :sass
+    config.generators do |g|
+      g.template_engine :slim
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
